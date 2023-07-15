4fed2a2b800d9652adfa4b5bc8d6223868e8076e
haterb4 <haterb2803@gmail.com>
2023-07-15 15:00:12 +0100

    backend routing

--- app/api/healthcheck/route.ts
+++ app/api/healthcheck/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: Request){
-      return NextResponse.json({ message: 'reCAPTCHA verification successful' })
+export async function GET(req: NextRequest){
+    return NextResponse.json({ message: 'reCAPTCHA verification successful' })
 }
