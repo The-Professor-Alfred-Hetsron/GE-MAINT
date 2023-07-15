b18cf40c589899f9d24bd3d460e750e2f13e3e41
The-Professor-Alfred-Hetsron <parkeryepnjio@gmail.com>
2023-07-15 10:41:13 +0100

    Update Starter

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,10 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'TYA MAINT',
   description: 'Gestion de Suivi et Maintenance des Groupes électrogenes pour le Service d’Exploitation et Maintenance des Réseaux Utilitaires du PAK',
+  icons: {
+    icon: '/Logo.svg',
+    shortcut: '/Logo.svg'
+  },
 }
 
 export default function RootLayout({
