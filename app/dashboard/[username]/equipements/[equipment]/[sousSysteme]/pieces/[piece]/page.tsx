fe1982470e5b30b05b45aa16efa74e2cf115ab90
The-Professor-Alfred-Hetsron <parkeryepnjio@gmail.com>
2023-07-23 21:18:25 +0100

    Update page.tsx

--- app/dashboard/[username]/equipements/[equipment]/[sousSysteme]/pieces/[piece]/page.tsx
+++ app/dashboard/[username]/equipements/[equipment]/[sousSysteme]/pieces/[piece]/page.tsx
@@ -4,13 +4,15 @@ import Link from "next/link"
 import Image from "next/image"
 import { usePathname } from 'next/navigation'
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
-import DeleteBtn from '../../../../../../../../components/UIElements/DeleteBtn'
 
-export default function Piece ({params}:{params: { piece: string }}) {
+import DeleteBtn from '@/components/UIElements/DeleteBtn'
+import UpdateBtn from '@/components/UIElements/UpdateBtn'
+
+export default function Piece ({params}:{params: {username: string, equipment:string, sousSysteme: string, piece: string }}) {
     const pathname = usePathname()
-    const subSysName = pathname.split('/')[pathname.split('/').length-3]
-    const equipmentName = pathname.split('/')[pathname.split('/').length-4]
-    const username = pathname.split('/')[pathname.split('/').length-6]
+    const subSysName = decodeURI(params.sousSysteme)
+    const equipmentName = decodeURI(params.equipment)
+    const username = decodeURI(params.username)
 
     const piece = {
         nom: "Nom Piece1",
@@ -29,14 +31,14 @@ export default function Piece ({params}:{params: { piece: string }}) {
     }
 
     return(
-        <div className="w-full bg-white rounded-2xl shadow backdrop-blur-[20px] p-2 flex-col justify-start items-center gap-2 flex">
+        <div className="w-full h-full bg-white rounded-2xl shadow backdrop-blur-[20px] p-2 flex-col justify-start items-center gap-2 flex">
             <div className="w-full justify-start items-center inline-flex gap-1">
                 <Link href={`/dashboard/${username}/equipements/${equipmentName}`} className="text-[#165081] text-[24px] font-semibold uppercase">{equipmentName.replace("-"," ")}</Link>
                 <Link href={`/dashboard/${username}/equipements/${equipmentName}/${subSysName}`} className="text-[#0B5DA7] text-[24px] font-semibold uppercase"> - {subSysName.replace("-"," ")}</Link>
                 <span className="text-zinc-800 text-[24px] font-semibold uppercase"> - Pièces de rechange</span>
             </div>
 
-            <div className="w-full p-2 bg-white rounded-2xl border border-slate-300 flex-col justify-start items-center gap-2.5 inline-flex">
+            <div className="w-full h-full p-2 bg-white rounded-2xl border border-slate-300 flex-col justify-start items-center gap-2.5 inline-flex">
                 <div className="w-full flex-row justify-start items-start gap-2 inline-flex">
                     <div className='w-full aspect-square bg-[#D0E5F0] rounded-[16px] flex justify-center items-center'>
                         <Image className="w-4/5 aspect-square" width="500" height="500" src={piece.image} alt={params.piece.replace("-", " ")}/>
@@ -46,42 +48,43 @@ export default function Piece ({params}:{params: { piece: string }}) {
                             <span className="text-black text-[26px] font-semibold uppercase">{piece.nom}</span>
                             <div className="justify-start items-center gap-[4px] inline-flex">
                                 <span className="text-black text-[18px] font-normal leading-loose">Marque du Fabricant: </span>
-                                <span className="text-black text-[20px] font-semibold uppercase">{piece.marque}</span>
+                                <span className="text-black text-[20px] font-semibold">{piece.marque}</span>
                             </div>
                             <div className="justify-start items-center gap-[4px] inline-flex">
                                 <span className="text-black text-[18px] font-normal leading-loose">Numéro de Série: </span>
-                                <span className="text-black text-[20px] font-semibold uppercase">{piece.numSerie}</span>
+                                <span className="text-black text-[20px] font-semibold">{piece.numSerie}</span>
                             </div>
                             <div className="justify-start items-center gap-[4px] inline-flex">
                                 <span className="text-black text-[18px] font-normal leading-loose">Modèle: </span>
-                                <span className="text-black text-[20px] font-semibold uppercase">{piece.modele}</span>
+                                <span className="text-black text-[20px] font-semibold">{piece.modele}</span>
                             </div>
                             <div className="justify-start items-center gap-[4px] inline-flex">
                                 <span className="text-black text-[18px] font-normal leading-loose">Localisation: </span>
-                                <span className="text-black text-[20px] font-semibold uppercase">{piece.localisation}</span>
+                                <span className="text-black text-[20px] font-semibold">{piece.localisation}</span>
                             </div>
                             <div className="justify-start items-center gap-[4px] inline-flex">
                                 <span className="text-black text-[18px] font-normal leading-loose">Quantité en Stock: </span>
-                                <span className="text-black text-[20px] font-semibold uppercase">{piece.qteStock}</span>
+                                <span className="text-black text-[20px] font-semibold">{piece.qteStock}</span>
                             </div>
                             <div className="justify-start items-center gap-[4px] inline-flex">
                                 <span className="text-black text-[18px] font-normal leading-loose">Quantité Minimale: </span>
-                                <span className="text-black text-[20px] font-semibold uppercase">{piece.qteMin}</span>
+                                <span className="text-black text-[20px] font-semibold">{piece.qteMin}</span>
                             </div>
                             <div className="justify-start items-baseline gap-[4px] inline-flex">
                                 <span className="text-black text-[18px] font-normal leading-loose">Description: </span>
-                                <span className="text-black text-[20px] font-semibold uppercase">{piece.description}</span>
+                                <span className="text-black text-[20px] font-semibold">{piece.description}</span>
                             </div>
                         </div>
-                        <div className="w-full pr-2 flex flex-wrap justify-between items-start">
+                        <div className="w-full pr-2 flex flex-wrap gap-2 justify-start items-center">
                             <DeleteBtn deleteAction={deletePiece}/>
-                            <button onClick={()=>deletePiece()} className='px-4 py-2 justify-center items-center gap-2 inline-flex rounded-lg bg-white border border-[#EDA92A] text-[#EDA92A] hover:bg-[#EDA92A] hover:border-none hover:text-white'>
-                                <AiOutlineMinus size={20}/>
-                                <span className='text-[16px] font-semibold'>Retirer du Stock</span>
-                            </button>
-                            <button onClick={()=>deletePiece()} className='px-4 py-2 justify-center items-center gap-2 inline-flex rounded-lg bg-white border border-[#34A853] text-[#34A853] hover:bg-[#34A853] hover:border-none hover:text-white'>
+                            <UpdateBtn updateAction={()=>{}}/>
+                            <button onClick={()=>deletePiece()} className='w-full px-4 py-2 justify-center items-center gap-2 inline-flex rounded-lg bg-white border border-[#34A853] text-[#34A853] hover:bg-[#34A853] hover:border-none hover:text-white'>
                                 <AiOutlinePlus size={20}/>
                                 <span className='text-[16px] font-semibold'>Ajouter en Stock</span>
+                            </button>
+                            <button onClick={()=>deletePiece()} className='w-full px-4 py-2 justify-center items-center gap-2 inline-flex rounded-lg bg-white border border-[#EDA92A] text-[#EDA92A] hover:bg-[#EDA92A] hover:border-none hover:text-white'>
+                                <AiOutlineMinus size={20}/>
+                                <span className='text-[16px] font-semibold'>Retirer du Stock</span>
                             </button>
                         </div>
                     </div>
