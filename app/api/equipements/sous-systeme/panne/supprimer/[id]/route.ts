a27819affe85d0ea7c775288c34844a12931a4bf
haterb4 <haterb2803@gmail.com>
2023-08-26 11:17:27 +0100

    delete panne

---
+++ app/api/equipements/sous-systeme/panne/supprimer/[id]/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server'
+import prismadb from '../../../../../../../lib/prismadb'
+
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+    try {
+        const id = params.id
+        const panne = await prismadb.panne.findUnique({
+            where: {
+                id: Number.parseInt(id)
+            }
+        });
+        if (!panne){
+            return NextResponse.json({ message: "panne innexistante" }, { status: 200 });
+        }
+        //supprimer la panne
+        await prismadb.panne.delete({
+            where: {
+                id: Number.parseInt(id),
+            },
+        });
+      return NextResponse.json({ message: "panne supprimee avec success" }, { status: 200 });
+    } catch (error) {
+      return NextResponse.json({ error: 'internal server error' }, { status: 500 })
+    }
+}
